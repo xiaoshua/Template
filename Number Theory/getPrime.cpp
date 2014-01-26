8752ee3b2447124a912f916344830de117ef8e5e
xiaoshua <809913474@qq.com>
2014-01-26 11:26:17 +0800

    summary
    desc

--- Number Theory/getPrime.cpp
+++ Number Theory/getPrime.cpp
@@ -1,11 +1,11 @@
 const int N = 1e7+9;
 int p[N/5], pn;
 bool notP[N];
-bool getP() {
+bool getP(int n=N) {
     int i, j;
-    for(i=2; i<N; ++i) {
+    for(i=2; i<n; ++i) {
         if(!notP[i]) p[pn++]=i;
-        for(j=0; j<pn && i*p[j]<N; ++j) {
+        for(j=0; j<pn && i*p[j]<n; ++j) {
             notP[i*p[j]]=true;
             if(i%p[j]==0) break;
         }
