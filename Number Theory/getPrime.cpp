e126a460d12e3ee64f1d22cc27651308d0757617
xiaoshua <809913474@qq.com>
2014-04-05 13:59:10 +0800

    a

---
+++ Number Theory/getPrime.cpp
@@ -0,0 +1,34 @@
+#include<map>
+#include<set>
+#include<queue>
+#include<stack>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<sstream>
+#include<iostream>
+#include<algorithm>
+#include<functional>
+using namespace std;
+const int N = 1e7 + 9;
+typedef long long LL;
+int p[N / 5], pn;
+bool notP[N];
+bool getP(int n = N) {
+	int i, j;
+	
+    for(i = 2; i < n; ++i) {
+		if(!notP[i]) {
+			p[pn++] = i;
+		}
+
+		for(j = 0; j < pn && i * p[j] < n; ++j) {
+			notP[i * p[j]] = true;
+
+			if(i % p[j] == 0) {
+				break;
+			}
+		}
+	}
+}
+
